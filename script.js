69e05dc14dbf03ceafb658a388c35fef3e3eee80
MisiriyaAddhra <misiriyaaddhra7@gmail.com>
2024-06-20 21:27:47 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,374 @@
+const data = [
+  {
+      id: 1,
+      name: "Mid Journey",
+      img: "./assets/images/1102970375731691612/f229a622417118ca41bbcddbd3aa12f4.jpg",
+      description: "The official server for Midjourney, a text-to-image AI where your imagination is the only limit.",
+      cat: "science & tech",
+      online: "1,66,900 Online",
+      members: "1,55,8800 Members",
+      icon:"./assets/icons/150074202727251969/d059a3821ea30f12baf1a5cda8a570e3.jpg",
+  },
+  {
+      id: 2,
+      name: "The Guild",
+      img: "./assets/images/1046979304547954728/d0ac123bc0db716e100a5a3a24c62fa8.jpg",
+      description:"The official server for Midjourney, a text-to-image AI where your imagination is the only limit.",
+      cat: "Education",
+      online: "1,66,900 Online",
+      members: "1,55,8800 Members",
+      icon:"./assets/icons/398627612299362304/a_792c266092d4989bb9a67e0abe3e965f.jpg",
+  },
+  {
+    id: 3,
+    name: "Royal",
+    img: "./assets/images/398627612299362304/4f1fa86af52e9c3af85e63605ec5748d.jpg",
+    description:"The official server for Midjourney, a text-to-image AI where your imagination is the only limit.",
+    cat: "Education",
+    online: "1,66,900 Online",
+    members: "1,55,8800 Members",
+    icon:"./assets/icons/522681957373575168/84a7500128d64ca60e959799c3e66f21.jpg",
+  },
+  {
+    id: 4,
+    name: "Dora",
+    img: "./assets/images/522681957373575168/5167dfbb5aa9b38c4696dc080360c826.jpg",
+    description:"The official server for Midjourney, a text-to-image AI where your imagination is the only limit.",
+    cat: "Entertainment",
+    online: "1,66,900 Online",
+    members: "1,55,8800 Members",
+    icon:"./assets/icons/541484311354933258/a_0f0bbb6cab6f28ce7d60e4818c4550e5.jpg",
+  },
+  {
+    id: 5,
+    name: "Aventures",
+    img: "./assets/images/541484311354933258/0e0519045645ed1931c408ec42af9000.jpg",
+    description:"The official server for Midjourney, a text-to-image AI where your imagination is the only limit.",
+    cat: "music",
+    online: "1,66,900 Online",
+    members: "1,55,8800 Members",
+    icon:"./assets/icons/562762757905186828/05d312e0df9dbae9d43273987a53a794.jpg",
+  },
+  {
+    id: 6,
+    name: "Rockstars",
+    img: "./assets/images/562762757905186828/e69c9585f8b23477c0626ac4a96f2794.jpg",
+    description:"The official server for Midjourney, a text-to-image AI where your imagination is the only limit.",
+    cat: "Entertainment",
+    online: "1,66,900 Online",
+    members: "1,55,8800 Members",
+    icon:"./assets/icons/586704051148816385/a_0373a32bd3c0a40c117a7dbfc541eaf1.jpg",
+  },
+  {
+    id:7 ,
+    name: "Chota Bheem",
+    img: "./assets/images/586704051148816385/8d9f4e10f9394ee7d8ba90bec94ef2a0.jpg",
+    description:"The official server for Midjourney, a text-to-image AI where your imagination is the only limit.",
+    cat: "gaming",
+    online: "1,66,900 Online",
+    members: "1,55,8800 Members",
+    icon:"./assets/icons/658675654799130624/55acb631979a46d3f94f63e8b8b9f4e7.jpg",
+  },
+  {
+    id:8 ,
+    name: " Bheem",
+    img: "./assets/images/658675654799130624/a1f0807229ba1d1af94c5685cb0b208e.jpg",
+    description:"The official server for Midjourney, a text-to-image AI where your imagination is the only limit.",
+    cat: "gaming",
+    online: "1,66,900 Online",
+    members: "1,55,8800 Members",
+    icon:"./assets/icons/662267976984297473/39128f6c9fc33f4c95a27d4c601ad7db.jpg",
+  },
+  {
+    id:9 ,
+    name: "The Rocky",
+    img: "./assets/images/679875946597056683/bb4791a77207e327dfa42c301a2d9603.jpg",
+    description:"The official server for Midjourney, a text-to-image AI where your imagination is the only limit.",
+    cat: "music",
+    online: "1,66,900 Online",
+    members: "1,55,8800 Members",
+    icon:"./assets/icons/679875946597056683/a_6103d052635b9c6d1b12ae7a018e1f25.jpg",
+  },
+  {
+    id: 10,
+    name: "Horror",
+    img: "./assets/images/710745950380884009/516d0293ef3c0ba1c2cfe361f04ec484.jpg",
+    description: "The official server for Midjourney, a text-to-image AI where your imagination is the only limit.",
+    cat: "science & tech",
+    online: "1,66,900 Online",
+    members: "1,55,8800 Members",
+    icon:"./assets/icons/1046979304547954728/bbe5d6097255431300cfc18dd629b876.jpg",
+},
+{
+    id: 11,
+    name: "The Guild",
+    img: "./assets/images/1046979304547954728/d0ac123bc0db716e100a5a3a24c62fa8.jpg",
+    description:"The official server for Midjourney, a text-to-image AI where your imagination is the only limit.",
+    cat: "Education",
+    online: "1,66,900 Online",
+    members: "1,55,8800 Members",
+    icon:"./assets/icons/398627612299362304/a_792c266092d4989bb9a67e0abe3e965f.jpg",
+},
+{
+  id: 12,
+  name: "Royal",
+  img: "./assets/images/398627612299362304/4f1fa86af52e9c3af85e63605ec5748d.jpg",
+  description:"The official server for Midjourney, a text-to-image AI where your imagination is the only limit.",
+  cat: "Education",
+  online: "1,66,900 Online",
+  members: "1,55,8800 Members",
+  icon:"./assets/icons/522681957373575168/84a7500128d64ca60e959799c3e66f21.jpg",
+},
+{
+  id: 13,
+  name: "Dora",
+  img: "./assets/images/522681957373575168/5167dfbb5aa9b38c4696dc080360c826.jpg",
+  description:"The official server for Midjourney, a text-to-image AI where your imagination is the only limit.",
+  cat: "Entertainment",
+  online: "1,66,900 Online",
+  members: "1,55,8800 Members",
+  icon:"./assets/icons/541484311354933258/a_0f0bbb6cab6f28ce7d60e4818c4550e5.jpg",
+},
+{
+  id: 15,
+  name: "Aventures",
+  img: "./assets/images/541484311354933258/0e0519045645ed1931c408ec42af9000.jpg",
+  description:"The official server for Midjourney, a text-to-image AI where your imagination is the only limit.",
+  cat: "science & tech",
+  online: "1,66,900 Online",
+  members: "1,55,8800 Members",
+  icon:"./assets/icons/562762757905186828/05d312e0df9dbae9d43273987a53a794.jpg",
+},
+{
+  id: 16,
+  name: "Rockstars",
+  img: "./assets/images/562762757905186828/e69c9585f8b23477c0626ac4a96f2794.jpg",
+  description:"The official server for Midjourney, a text-to-image AI where your imagination is the only limit.",
+  cat: "Entertainment",
+  online: "1,66,900 Online",
+  members: "1,55,8800 Members",
+  icon:"./assets/icons/586704051148816385/a_0373a32bd3c0a40c117a7dbfc541eaf1.jpg",
+},
+{
+  id:17 ,
+  name: "Chota Bheem",
+  img: "./assets/images/586704051148816385/8d9f4e10f9394ee7d8ba90bec94ef2a0.jpg",
+  description:"The official server for Midjourney, a text-to-image AI where your imagination is the only limit.",
+  cat: "gaming",
+  online: "1,66,900 Online",
+  members: "1,55,8800 Members",
+  icon:"./assets/icons/658675654799130624/55acb631979a46d3f94f63e8b8b9f4e7.jpg",
+},
+{
+  id:18 ,
+  name: " Bheem",
+  img: "./assets/images/658675654799130624/a1f0807229ba1d1af94c5685cb0b208e.jpg",
+  description:"The official server for Midjourney, a text-to-image AI where your imagination is the only limit.",
+  cat: "gaming",
+  online: "1,66,900 Online",
+  members: "1,55,8800 Members",
+  icon:"./assets/icons/662267976984297473/39128f6c9fc33f4c95a27d4c601ad7db.jpg",
+},
+{
+  id:19 ,
+  name: "The Rocky",
+  img: "./assets/images/679875946597056683/bb4791a77207e327dfa42c301a2d9603.jpg",
+  description:"The official server for Midjourney, a text-to-image AI where your imagination is the only limit.",
+  cat: "music",
+  online: "1,66,900 Online",
+  members: "1,55,8800 Members",
+  icon:"./assets/icons/679875946597056683/a_6103d052635b9c6d1b12ae7a018e1f25.jpg",
+},
+{
+  id: 21,
+  name: "Mid Journey",
+  img: "./assets/images/1102970375731691612/f229a622417118ca41bbcddbd3aa12f4.jpg",
+  description: "The official server for Midjourney, a text-to-image AI where your imagination is the only limit.",
+  cat: "science & tech",
+  online: "1,66,900 Online",
+  members: "1,55,8800 Members",
+  icon:"./assets/icons/150074202727251969/d059a3821ea30f12baf1a5cda8a570e3.jpg",
+},
+{
+  id: 22,
+  name: "The Guild",
+  img: "./assets/images/1046979304547954728/d0ac123bc0db716e100a5a3a24c62fa8.jpg",
+  description:"The official server for Midjourney, a text-to-image AI where your imagination is the only limit.",
+  cat: "Education",
+  online: "1,66,900 Online",
+  members: "1,55,8800 Members",
+  icon:"./assets/icons/398627612299362304/a_792c266092d4989bb9a67e0abe3e965f.jpg",
+},
+{
+id: 23,
+name: "Royal",
+img: "./assets/images/398627612299362304/4f1fa86af52e9c3af85e63605ec5748d.jpg",
+description:"The official server for Midjourney, a text-to-image AI where your imagination is the only limit.",
+cat: "Education",
+online: "1,66,900 Online",
+members: "1,55,8800 Members",
+icon:"./assets/icons/522681957373575168/84a7500128d64ca60e959799c3e66f21.jpg",
+},
+{
+id: 24,
+name: "Dora",
+img: "./assets/images/522681957373575168/5167dfbb5aa9b38c4696dc080360c826.jpg",
+description:"The official server for Midjourney, a text-to-image AI where your imagination is the only limit.",
+cat: "Entertainment",
+online: "1,66,900 Online",
+members: "1,55,8800 Members",
+icon:"./assets/icons/541484311354933258/a_0f0bbb6cab6f28ce7d60e4818c4550e5.jpg",
+},
+{
+id: 25,
+name: "Aventures",
+img: "./assets/images/541484311354933258/0e0519045645ed1931c408ec42af9000.jpg",
+description:"The official server for Midjourney, a text-to-image AI where your imagination is the only limit.",
+cat: "music",
+online: "1,66,900 Online",
+members: "1,55,8800 Members",
+icon:"./assets/icons/562762757905186828/05d312e0df9dbae9d43273987a53a794.jpg",
+},
+{
+id: 26,
+name: "Rockstars",
+img: "./assets/images/562762757905186828/e69c9585f8b23477c0626ac4a96f2794.jpg",
+description:"The official server for Midjourney, a text-to-image AI where your imagination is the only limit.",
+cat: "Entertainment",
+online: "1,66,900 Online",
+members: "1,55,8800 Members",
+icon:"./assets/icons/586704051148816385/a_0373a32bd3c0a40c117a7dbfc541eaf1.jpg",
+},
+{
+id:27 ,
+name: "Chota Bheem",
+img: "./assets/images/586704051148816385/8d9f4e10f9394ee7d8ba90bec94ef2a0.jpg",
+description:"The official server for Midjourney, a text-to-image AI where your imagination is the only limit.",
+cat: "gaming",
+online: "1,66,900 Online",
+members: "1,55,8800 Members",
+icon:"./assets/icons/658675654799130624/55acb631979a46d3f94f63e8b8b9f4e7.jpg",
+},
+{
+id:8 ,
+name: " Bheem",
+img: "./assets/images/658675654799130624/a1f0807229ba1d1af94c5685cb0b208e.jpg",
+description:"The official server for Midjourney, a text-to-image AI where your imagination is the only limit.",
+cat: "gaming",
+online: "1,66,900 Online",
+members: "1,55,8800 Members",
+icon:"./assets/icons/662267976984297473/39128f6c9fc33f4c95a27d4c601ad7db.jpg",
+},
+{
+id:9 ,
+name: "The Rocky",
+img: "./assets/images/679875946597056683/bb4791a77207e327dfa42c301a2d9603.jpg",
+description:"The official server for Midjourney, a text-to-image AI where your imagination is the only limit.",
+cat: "music",
+online: "1,66,900 Online",
+members: "1,55,8800 Members",
+icon:"./assets/icons/679875946597056683/a_6103d052635b9c6d1b12ae7a018e1f25.jpg",
+},
+ 
+];
+
+const productsContainer = document.querySelector(".boxes");
+const categoriesContainer = document.querySelector(".cats");
+const searchInput = document.querySelector(".search");
+const resultsCount = document.querySelector(".results-count");
+
+const displayProducts = (products) => {
+  productsContainer.innerHTML = products
+      .map(
+          (product) => `
+      <div class="box">
+          <img src=${product.img} alt=""/>
+          <div class="content">
+              <div class="top-line">
+                  <span><img src=${product.icon} alt="img"/></span>
+                  <h3>${product.name}</h3>
+              </div>
+              <p>${product.description}</p>
+              <ul>
+                  <li>${product.online}</li>
+                  <li class="list">${product.members}</li>
+              </ul>
+          </div>
+      </div>`
+      )
+      .join("");
+  resultsCount.textContent = `${products.length} Results Found`;
+};
+
+
+const setCategories = () => {
+  displayProducts(data);
+
+  const allCats = data.map((item) => item.cat);
+  const categories = ["All", ...new Set(allCats)];
+
+  categoriesContainer.innerHTML = categories
+    .map(
+      (cat) => `
+      <li class="cat">
+          <a href="#">
+            <i class="fa fa-${getIconClass(cat)}"></i> ${cat}
+          </a>
+          <h6 class="count">${cat === "All" ? data.length : data.filter((item) => item.cat === cat).length}</h6>
+      </li>`
+    )
+    .join("");
+
+  categoriesContainer.addEventListener("click", (e) => {
+    if (e.target.tagName === "A") {
+      const selectedCat = e.target.textContent.trim();
+      const filteredProducts =
+        selectedCat === "All"
+          ? data
+          : data.filter((item) => item.cat === selectedCat);
+      displayProducts(filteredProducts);
+    }
+  });
+};
+
+const getIconClass = (category) => {
+  switch (category.toLowerCase()) {
+    case "gaming":
+      return "gamepad";
+    case "education":
+      return "graduation-cap";
+    case "entertainment":
+      return "tv";
+    case "all":
+      return "compass";
+    case "music":
+      return "music";
+    case "science & tech": 
+      return "atom";
+    default:
+      return "compass"; 
+  }
+};
+
+
+const filterProducts = (searchTerm) => {
+  const filteredProducts = data.filter((item) =>
+      item.name.toLowerCase().includes(searchTerm.toLowerCase())
+  );
+  displayProducts(filteredProducts);
+};
+
+searchInput.addEventListener("keyup", (e) => {
+  filterProducts(e.target.value);
+});
+
+
+
+
+displayProducts(data);
+
+setCategories();
+
+
+
+
